43fdb4b87b4d50812bab06bd54fbe2f9efd91fd6
userMarcPaul <rjsanoy08@gmail.com>
2024-11-09 09:50:53 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.home-text', {delay:200, origin:'top'});
+sr.reveal('.home-img', {delay:450, origin:'top'});
+sr.reveal('.scroll-down', {delay:500, origin:'right'});
